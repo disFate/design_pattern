7ced56eb73fe9f30f9f57ea29c1530bfd7f0f77e
disFate <94330699+disFate@users.noreply.github.com>
2022-12-10 22:48:18 -0500

    strategy pattern

---
+++ strategy/src/test/java/test.java
@@ -0,0 +1,11 @@
+import com.google.Duck1;
+import org.junit.Test;
+
+public class test {
+    @Test
+    public void testDuck() {
+        Duck1 duck = new Duck1();
+        duck.performFly();
+        duck.performQuack();
+    }
+}
